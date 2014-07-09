269652611248f9d877bb2f54421138fb6fddba74
marinaelmore <marina.elmore@gmail.com>
2014-07-09 14:14:45 -0700

    iniital commit

---
+++ nodelink.js
@@ -0,0 +1,35 @@
+window.onload = function () {
+    var data = {
+        nodes: [
+            {value: 10, group: 'a', name: 'node 1'},
+            {value: 20, group: 'a', name: 'node 2'},
+            {value: 30, group: 'b', name: 'node 3'},
+            {value: 40, group: 'b', name: 'node 4'},
+            {value: 50, group: 'c', name: 'node 5'},
+            {value: 60, group: 'c', name: 'node 6'},
+            {value: 70, group: 'd', name: 'node 7'},
+            {value: 100, group: 'e', name: 'node 8'},
+        ],
+        links: [
+            {s: 0, t: 1, v: 10},
+            {s: 0, t: 7, v: 1},
+            {s: 1, t: 2},
+            {s: 2, t: 3},
+            {s: 3, t: 4},
+            {s: 4, t: 5},
+            {s: 5, t: 6},
+            {s: 6, t: 0}
+        ]
+    };
+
+    $("#content").nodelink({
+        data: data,
+        nodeCharge: {value: -400},
+        linkSource: {field: "s"},
+        linkTarget: {field: "t"},
+        linkSize: {field: "v"},
+        nodeSize: {field: "value"},
+        nodeColor: {field: "group"},
+        nodeLabel: {field: "name"}
+    });
+};
